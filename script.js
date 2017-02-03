efb8cdb33191ff8afe5a6f39bcb05e921ab31807
iGodShark <kaplunbs@dcsdk12.org>
2017-02-03 10:12:54 -0700

    Removed files, Added a file, changed html, styling, and javascript.
    I removed 1,2, and 3.html and added student.html for only 1 page for
    future purposes. I changed some styling in style.css for my needs on
    password.html. I removed login.css for it is not needed anymore. I
    edited a lot of javascript so that you can now create and remove codes.
    And I set a limit on the amount of codes (1-99).

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+var Codes;
+
 function SubmitPassword(){
 	var EnteredPassword = document.getElementById("Password");
 	var PassRef = firebase.database().ref().child("Password");
 	PassRef.on('value', function(data){
 		if(EnteredPassword.value == data.val()){
 			//Code to be run if Password is correct.
-			location.replace("console.html");
+			var mainConsole = document.getElementById("mainConsole");
+			document.getElementById("passH1").innerHTML = "Administrator Console";
+			document.getElementById("passwordTextBox").style.display = "none";
+			mainConsole.style.display = "block";
+			/*mainConsoleRef = firebase.database().ref().child("mainConsole");
+			mainConsoleRef.on('value', function(data2){
+				mainConsole.innerHTML = data2.val();
+			});*/
+			firebase.database().ref().child("Codes").on('value', function(data){
+				Codes = data.val();
+				Update();
+			});
 		}
 		else{
 			//Code to be run if Password is incorrect.
@@ -12,4 +25,31 @@ function SubmitPassword(){
 			EnteredPassword.value = "";
 		}
 	});
+}
+
+function Update(){
+	for(var i = 0; i < Codes.length - 1; i++){
+		document.getElementById("Codes").innerHTML =
+		document.getElementById("Codes").innerHTML +
+		"<p>Code " + (i + 1) + ": <input value='" + Codes[i + 1] +
+		"' id='Code" + i + "'></p>";
+	}
+}
+
+function plusCode(){
+	if(Codes.length < 100){
+		document.getElementById("Codes").innerHTML = "";
+		firebase.database().ref().child("Codes").child(Codes.length).set("sample code");
+	}
+}
+
+function minCode(){
+	if(Codes.length > 2){
+		document.getElementById("Codes").innerHTML = "";
+		firebase.database().ref().child("Codes").child(Codes.length - 1).remove();
+	}
+}
+
+function Save(){
+	window.location.replace("password.html");
 }
